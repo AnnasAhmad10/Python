3f9a0c8a63f900eea38011c9d7dae1b9c82d8a57
AnnasAhmad10 <anasjan41@gmail.com>
2024-09-16 22:35:10 +0500

    Dictionary and sets

---
+++ lecture 4/lecture_4.py
@@ -0,0 +1,53 @@
+meaning ={
+    "name" : "annas",
+    "class" : "llb",
+    "subjects" : ('pil', 'pil2'),
+    "marks" : ["56", "26", "32"],
+}
+
+meaning["name"] = "jan sb"
+meaning["surname"] = "akhunzada"
+
+
+#nested dictionary
+meaning1 ={
+    "name" : "annas",
+    "class" : "llb",
+    "subjects" : {
+        "pil" : 67,
+        "pil2" : 88,
+        "hr" : 78,
+    },
+    "marks" : ["56", "26", "32"],
+}
+ 
+ #dictionary methods
+print(meaning1.keys())
+
+print(meaning1.values())
+
+student ={
+    "name" : "annas",
+    "class" : "llb",
+    "subjects" : {
+        "pil" : 67,
+        "pil2" : 88,
+        "hr" : 78,
+    },
+    "marks" : ["56", "26", "32"],
+}
+print(student)
+new_dic = {"unit" : "A block"}
+student.update(new_dic)
+print(student)
+
+sets
+version = {2,3,3,4,5}
+print(type(version))
+version.pop()
+print(version)
+
+set1 = {1,2,3,4,5}
+set2 = {4,5,6,7,8}
+print(set1.union(set2))
+print(set1.intersection(set2))
